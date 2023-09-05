70cc3bbdbde09353cdc366e6c7d2e4d720644094
thel3onardo <leonardo.contato03@gmail.com>
2023-09-05 16:06:47 -0300

    chore: define design system's colors and font family

---
+++ lib/config/themes/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData darkTheme = ThemeData(
+    primaryColor: const Color(0xff9775FA),
+    primaryColorDark: const Color(0xff1D1E20),
+    primaryColorLight: const Color(0xffF5F6FA),
+    useMaterial3: true,
+    fontFamily: "Inter");
